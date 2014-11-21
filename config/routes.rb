2bec7ef475ec0081361938afe4f8ae61158693f1
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-21 13:52:35 -0300

    Added ads creation, links and other layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get '/about',    to: 'pages#about'
-  get '/login',    to: 'sessions#new'
-  get '/register', to: 'users#new'
+  get '/about',      to: 'pages#about'
+  get '/login',      to: 'sessions#new'
+  get '/ad-example', to: 'ads#show'
 
-  get '/ad-example', to:'ads#show'
-
-  resources :ads
-  resources :users
+  resources :ads,   only: [:index, :new]
+  resources :users, only: [:new, :show, :edit, :update]
 end
